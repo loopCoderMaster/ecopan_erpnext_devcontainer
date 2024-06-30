ebb66630f6ed59e60e7076ca11f525c5216a5d9a
loopCoderMaster <132823027+loopCoderMaster@users.noreply.github.com>
2024-06-30 18:34:56 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,4 +4,4 @@ wget https://raw.githubusercontent.com/frappe/frappe_docker/main/development/ins
 chmod +x installer.py
 echo "[]" > apps-example.json
 ./installer.py
-sudo chown -R frappe:frappe /home/frappe/benches
+#sudo chown -R frappe:frappe /home/frappe/benches
