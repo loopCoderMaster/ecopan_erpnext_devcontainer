2a1de802202a8aef4b184197156ca39563c1c274
loopCoderMaster <132823027+loopCoderMaster@users.noreply.github.com>
2024-06-30 12:43:43 +0000

    first commit

---
+++ scripts/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo chown -R frappe:frappe /home/frappe/benches
+wget https://raw.githubusercontent.com/frappe/frappe_docker/main/development/installer.py
+chmod +x installer.py
+echo "[
+    {
+      "url": "https://github.com/frappe/erpnext.git",
+      "branch": "version-15"
+    }
+]" > apps-example.json
+./installer.py
