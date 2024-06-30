7cd5fcc2060093f61220b2bddb54e7d161e6bd3f
loopCoderMaster <132823027+loopCoderMaster@users.noreply.github.com>
2024-06-30 18:20:04 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,9 +2,6 @@
 
 wget https://raw.githubusercontent.com/frappe/frappe_docker/main/development/installer.py
 chmod +x installer.py
-echo "[
-  { \"url\": \"https://github.com/frappe/frappe.git\", \"branch\": \"version-15\" },
-  { \"url\": \"https://github.com/frappe/erpnext.git\", \"branch\": \"version-15\" }
-]" > apps-example.json
+echo "[]" > apps-example.json
 ./installer.py
 sudo chown -R frappe:frappe /home/frappe/benches
