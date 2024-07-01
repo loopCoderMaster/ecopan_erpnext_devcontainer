5a4127d6e8e97b59a697365d1af1002db3331b54
loopCoderMaster <132823027+loopCoderMaster@users.noreply.github.com>
2024-07-01 17:02:11 +0200

    Update postCreateCommand.sh

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -3,12 +3,12 @@
 echo "Start setup.sh !!!!"
 
 # Aggiorna i pacchetti di sistema
-apt-get update && apt-get upgrade -y
+# apt-get update && apt-get upgrade -y
 
 # Installa le dipendenze necessarie (se non sono già presenti)
-apt-get install -y \
-    wget \
-    curl #\
+#apt-get install -y \
+#    wget \
+#    curl #\
     #git \
     #python3 \
     #python3-pip \
@@ -32,15 +32,25 @@ chmod +x installer.py
 # Esegui lo script di installazione di Frappe
 ./installer.py # Aggiungi --verbose per vedere l'output dettagliato
 
+# Controlla l'esito dell'installer
+if [ $? -ne 0 ]; then
+  echo "Errore durante l'installazione di Frappe. Interruzione dello script."
+  exit 1  # Esci con un codice di errore
+fi
+
 rm installer.py apps-example.json
 
-if [ $? -eq 0 ]; then  # Verifica se l'installer è terminato con successo (codice di uscita 0)
-    sudo chown -R frappe:frappe /workspace/development/frappe-bench
-    cd /workspace/development/frappe-bench
-    bench --site development.localhost clear-cache
-    bench use development.localhost
-    bench browse
-    echo "Installazione completata. Frappe ed ERPNext dovrebbero essere in esecuzione."
-else
-    echo "Errore durante l'installazione. I file temporanei non sono stati rimossi."
-fi
+# Attesa opzionale per l'avvio dei servizi
+sleep 10
+
+sudo chown -R frappe:frappe /workspace/development/frappe-bench
+cd /workspace/development/frappe-bench
+
+echo "Clearing cache and setting site..."
+bench --site development.localhost clear-cache
+
+echo "Starting Frappe development server..."
+bench use development.localhost
+bench browse
+
+echo "Frappe development server avviato. Apri http://localhost:8000 nel tuo browser."
